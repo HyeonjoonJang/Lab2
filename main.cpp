853f6d9a39ea12cf61f2a87123702f7e59a65afc
HyeonJoon <jjhhjj0206@snu.ac.kr>
2025-10-07 16:59:55 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -5,18 +5,39 @@ using namespace std;
 
 int N,M;
 vector<int> vec;
+int arr[8];
+bool visited[8];
 
-void func(int cnt) {
+void func(int cnt)
+{
+    if(cnt == M)
+    {
+        for(int i = 0; i < vec.size(); i++)
+            cout << vec[i] << " ";
+        cout << endl;
+        return;
+    }
 
+    for(int i = 0; i < N; i++)
+    {
+        if(visited[i])  continue;
 
-
-
-
-
-
+        visited[i] = true;
+        vec.push_back(arr[i]);
+        func(cnt + 1);
+        vec.pop_back();
+        visited[i] = false;
+    }
 }
 
-int main() {
-   cin >> N >> M;
-   func(0);
+int main()
+{
+    cin >> N >> M;
+    for(int i = 0; i < N; i++)
+    {
+        arr[i] = i + 1;
+        visited[i] = false;
+    }
+
+    func(0);
 }
